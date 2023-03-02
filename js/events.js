39f389f577631c9faa59cc1cfb0da17dabd98449
Leandro J Martinez <leandro666m@gmail.com>
2023-03-01 23:14:45 -0300

    indexfilter funciona. seguir con upc-past

--- js/events.js
+++ js/events.js
@@ -1,44 +1,16 @@
-var loadCard = {
-  
-  all: () => { document.getElementById("row-cards").innerHTML = data.events.map( (cadaEvento) => cardTemplate(cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price)  ).join(" ") } ,
-  
-  upcoming: () => {
+function index(){
+  document.getElementById("row-cards").innerHTML = data.events.map( (cadaEvento) => cardTemplate(cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price)  ).join(" ") 
+} 
+
+function upcoming() {
     document.getElementById("row-cards").innerHTML = data.events.map( 
       cadaEvento => (new Date(cadaEvento.date)) < (new Date(data.currentDate)) ? cardTemplate( cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price) : "" ).join("") 
   }
-   , 
-  past: () => {
+    
+  function past() {
     document.getElementById("row-cards").innerHTML =  data.events.map( 
       cadaEvento => (new Date(cadaEvento.date)) > (new Date(data.currentDate)) ? cardTemplate( cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price) : "" ).join("") 
   }
-}
-
-function all(){
-  loadCard.all()
-} 
-
-function indexFilter(dataToSearch){
-  console.log(" function all(dataToSearch){ ");  console.log( dataToSearch.categories );
-
-/*   document.getElementById("row-cards").innerHTML = data.events.map( 
-            cadaEvento => ( cadaEvento.category == dataToSearch.categories ) ? cardTemplate(cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price) :"" 
-        ).join(" ")  
-} */
 
 
-/* document.getElementById("row-cards").innerHTML = 
-  (data.events.filter( evento => evento.category==dataToSearch.categories ) )
-  .map( each => cardTemplate(each._id, each.image, each.name, each.description, each.price)).join(" ")  
- */
-
-  dataToSearch.categories.forEach(element => {
-    console.log( element);
-    data.events.forEach( ev =>  )
-
-  });
-  
-    
-  
- 
-}
 
--- js/filters_search.js
+++ js/filters_search.js
@@ -1,24 +1,44 @@
 
 var dataToSearch
 
-function captureData(){
+function captureData() {
   let inputSearch = document.getElementById(`search`)
   let checks = document.querySelectorAll(`.checks:checked`)
-  
-  dataToSearch = {
-    [inputSearch.name] : inputSearch.value,
-    [checks[0].name]: []
-  }  
 
-  checks.forEach( each => dataToSearch[checks[0].name].push(each.value) )
-        console.log("en capturedata: "); console.log( dataToSearch );
+  dataToSearch = {
+    [inputSearch.name]: inputSearch.value,
+    [checks[0].name]: []
   }
+
+  checks.forEach(each => dataToSearch[checks[0].name].push(each.value))
+  console.log("en capturedata: "); console.log(dataToSearch);
+}
 
 
 let searchButton = document.getElementById(`searchButton`)
     searchButton.addEventListener(`click`, (evento) => {
       evento.preventDefault()
-      captureData()    
-      indexFilter( dataToSearch)
+      captureData()
+      indexFilter(dataToSearch)
     }
-    )
+)
+
+/* function indexFilter(dataToSearch){
+  var array = []
+   for (const elem of dataToSearch.categories) {
+      array.push( data.events.map( cadaEvento => ( cadaEvento.category == elem ) ? 
+           cardTemplate( cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price) : "" ).join("")
+      )
+   }
+   document.getElementById("row-cards").innerHTML = array.join("")
+ } */
+ 
+ function indexFilter(dataToSearch){
+  var array = []
+   for (const elem of dataToSearch.categories) {
+      array.push( 
+        data.events.map(cadaEvento=>(cadaEvento.category==elem) ? cardTemplate( cadaEvento._id, cadaEvento.image, cadaEvento.name, cadaEvento.description, cadaEvento.price) : "" ).join("")
+      )
+   }
+   document.getElementById("row-cards").innerHTML = array.join("")
+ }
