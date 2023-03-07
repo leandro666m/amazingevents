38dd844845b8690e1cf07187a96e4f6825b5d2fd
Leandro J Martinez <leandro666m@gmail.com>
2023-03-07 17:25:38 -0300

    task3 terminado

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,10 @@
 
 ( function () {
-
-
     var forms = document.querySelectorAll('.needs-validation')
 
     /* let array_form = Array.prototype.slice.call(forms) */
     let array_form = Array.from(forms)
-    console.log(array_form);
+
         array_form.forEach( each => each.addEventListener('submit', event => {
                                                                         if ( each.checkValidity() ) {
                                                                             Swal.fire({ 'title': 'Enviado',
