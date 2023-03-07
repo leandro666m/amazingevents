075015001507c5d9712b1637ea351f2686f716f7
Leandro J Martinez <leandro666m@gmail.com>
2023-03-06 21:02:37 -0300

    contact form validando y alertando

--- js/contact.js
+++ js/contact.js
@@ -1,39 +1,35 @@
+// para deshabilitar el envío de formularios si hay campos no válidos
+( function () {
+    'use strict'
+      // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Bucle sobre ellos y evitar el envío
+    let array_form = Array.prototype.slice.call(forms)
+        array_form.forEach( (each) => {
+            each.addEventListener('submit'
+                                    , (event) => {
+                                                if (!each.checkValidity()) {
+                                                    event.preventDefault()
+                                                    event.stopPropagation()
+                                                }else{  
+                                                    Swal.fire({
+                                                        'title': 'Enviado',
+                                                        'text': `A la brevedad nos pondremos en contacto.`,
+                                                        'icon': 'success'
+                                                        });
+                                                        event.preventDefault()
+                                                        event.stopPropagation()
+                                                }
+                                        
+                                                each.classList.add('was-validated')
+                                                }
+                                    )
+        } )
+        
+        
+        
+        
+  }
+) ()
 
-//document.querySelector('#btn-submit').addEventListener('click', obtenerDatos())
-
-function obtenerDatos(){
-    let error = validar();
-    if (error) {
-        Swal.fire({
-            'title': 'Sus datos no se pudieron enviar',
-            'text': 'Por favor revise los campos resaltados',
-            'icon': 'warning'
-        });
-    } else {
-        Swal.fire({
-            'title': 'Enviado',
-            'text': `A la brevedad nos pondremos en contacto.`,
-            'icon': 'success'
-            }).then(() => limpiar() );
-    };
-
-}
-function validar(){
-let inputs_requeridos = document.querySelectorAll('#frm-control [required]');
-let error = true;
-for (let i = 0; i < inputs_requeridos.length; i++) {
-    if (inputs_requeridos[i].value == '') {
-        inputs_requeridos[i].classList.add('input-error');
-        error = false;
-    } else {
-        inputs_requeridos[i].classList.remove('input-error');
-    }
-}
-
-return error;
-}
-function limpiar(){
-    form4Example1.value="";
-    form4Example2.value="";
-    form4Example3.value="";
-}
