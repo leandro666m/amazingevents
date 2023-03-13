c63646ac14c4217096966b7ab58422c92aba03cb
Leandro J Martinez <leandro666m@gmail.com>
2023-03-13 18:50:50 -0300

    task4-terminado-tabla de estadisticas

--- js/stats.js
+++ js/stats.js
@@ -11,22 +11,17 @@ let upcoming
 
 (async function getData(){
   await fetch("https://mh.up.railway.app/api/amazing-events")
-    .then( resp => resp.json() )
-    .then( json =>  {
+    .then( resp => resp.json() ).then( json =>  {
                       data = json.events;
                       date = json.currentDate.slice(0,10); 
                     }
             ) 
   
-            /* Past  */
-    await fetch("https://mh.up.railway.app/api/amazing-events?time=past")
-    .then( resp => resp.json() )
-    .then( json => past = json.events ) 
+    /* Past  */
+    await fetch("https://mh.up.railway.app/api/amazing-events?time=past").then( resp => resp.json() ).then( json => past = json.events ) 
   
     /* Upcoming  */
-    await fetch("https://mh.up.railway.app/api/amazing-events?time=upcoming")
-    .then( resp => resp.json() )
-    .then( json => upcoming = json.events ) 
+    await fetch("https://mh.up.railway.app/api/amazing-events?time=upcoming").then( resp => resp.json() ).then( json => upcoming = json.events ) 
 
     /* Events with highest percentage of attendance */
     let sortedPastAtt = [...past].sort((a, b) => b.assistance - a.assistance);
@@ -38,50 +33,58 @@ let upcoming
         document.querySelector('#max-cap').innerHTML = sortedPastCapac[0].capacity
 
         
-        /* Upcoming events statistics by category
-        Categories - Revenues - Percentage of attendance 
-        categoria - ganancia por categ - ( 100 * asist total estimada / capacidad  ) */
-        
         let categorias = [] //array de categorias de eventos SIN repetirse
         data.forEach( each => { !categorias.includes(each.category) && categorias.push(each.category) } )
         
-        /* por cada categoria hacer:  */
-        for (const categoria of categorias) {
 
+
+        /* por cada categoria hacer:  */
+        let stat_category_Up=[]
+        let stat_category_Past=[]
+        for (const categoria of categorias) {
+                    /* Upcoming events statistics by category
+                    Categories - Revenues - Percentage of attendance 
+                    categoria - ganancia por categ - ( 100 * asist total estimada / capacidad  ) */
             /* ganancia total estimada */
-            let acumRevenue_Categoria = upcoming.reduce( (acc , val) => { val.category == categoria ?  acc += val.price*val.estimate  : ""
+            let acumRevenue_Cat_Up = upcoming.reduce( (acc , val) => { val.category == categoria ?  acc += val.price*val.estimate  : ""
             return acc },  0 )
 
-
-            let acumCapacidad_Categoria = upcoming.reduce( (acc , val) => { val.category == categoria ?  acc += val.capacity  : "" 
+            let acumCapacidad_Cat_Up = upcoming.reduce( (acc , val) => { val.category == categoria ?  acc += val.capacity  : "" 
                                                     return acc },  0 )
-            let acumEstimado_Categoria = upcoming.reduce( (acc , val) => {val.category == categoria ?  acc += val.estimate  : ""
+            let acumEstimado_Cat_Up = upcoming.reduce( (acc , val) => {val.category == categoria ?  acc += val.estimate  : ""
                                                     return acc },  0 )
             /* porcentaje de asistencia */
-            let perc_att = 100 * acumEstimado_Categoria / acumCapacidad_Categoria
-            
-            
-            let stat_category = [
-                categoria,
-                acumRevenue_Categoria, 
-                perc_att
-            ]
+            let perc_att = 100 * acumEstimado_Cat_Up / acumCapacidad_Cat_Up
 
-                       
-            console.log(stat_category);
-       document.querySelector("#upcomingStat").innerHTML = stat_category.map(each=>
-        `<tr>
-                                    <td> ${categoria} </td>
-                                    <td> ${each.acumRevenue_Categoria} </td>
-                                    <td> ${each.perc_att} </td>
-                                </tr>`
-        )
-        
+            !isNaN(perc_att) && stat_category_Up.push(
+                `<tr>
+                    <td> ${categoria} </td>
+                    <td> ${acumRevenue_Cat_Up} </td>
+                    <td> ${perc_att.toFixed(2)}% </td>
+                </tr>`
+            ) 
 
 
-        
+            /* ganancia total */
+            let acumRevenue_Cat_Past = past.reduce( (acc , val) => { (val.category == categoria) && (acc += val.price*val.assistance) ;return acc },  0 )
 
+            let acumCapacidad_Cat_Past = past.reduce( (acc , val) => { (val.category == categoria) && (acc += val.capacity); return acc },  0 )
+            let acumEstimado_Cat_Past = past.reduce( (acc , val) => { (val.category == categoria) && (acc += val.assistance) ; return acc },  0 )
+            /* porcentaje de asistencia */
+            let perc_att_Past = 100 * acumEstimado_Cat_Past / acumCapacidad_Cat_Past
+
+
+            !isNaN(perc_att_Past) && stat_category_Past.push(
+                `<tr>
+                    <td> ${categoria} </td>
+                    <td> ${acumRevenue_Cat_Past} </td>
+                    <td> ${perc_att_Past.toFixed(2)}% </td>
+                </tr>`
+            ) 
        } /* for of */
+
+       document.querySelector("#upcomingStat").innerHTML = stat_category_Up.join("")
+       document.querySelector("#pastStat").innerHTML = stat_category_Past.join("")
     }
        )()
 
